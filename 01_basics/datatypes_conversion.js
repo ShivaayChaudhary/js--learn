227c57c5c89f2926f2016bd954cf56214d27de48
ShivaayChaudhary <cse22084@glbitm.ac.in>
2025-03-15 07:35:24 +0000

    conversion of datatypes

---
+++ 01_basics/datatypes_conversion.js
@@ -0,0 +1,14 @@
+let score=33
+let a="3ab"
+
+console.log(typeof(score))
+
+let stringscore=String(score)
+ console.log(typeof stringscore)
+ console.log( stringscore)
+ 
+ let b=Number(a);
+ console.log(typeof b);
+ console.log( b); //it will show NaN(Not a Number)
+ 
+ 

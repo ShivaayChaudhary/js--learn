7e943f451424968cc2923bb16c1ee58307a9dc63
ShivaayChaudhary <cse22084@glbitm.ac.in>
2025-03-15 08:01:42 +0000

    comparison operators

---
+++ 01_basics/comparison.js
@@ -0,0 +1,23 @@
+
+console.log("2">1);
+console.log("02">1); //string convert into numeric
+
+console.log("null");
+console.log(null>0);
+console.log(null<0);
+console.log(null==0); //equality check and comparisons like <,>,<=,>= works different
+console.log(null>=0); //comparisons treat null as 0.
+console.log(null<=0);
+
+console.log("undefined");
+console.log(undefined>0);
+console.log(undefined<0);
+console.log(undefined==0);
+console.log(undefined>=0);
+console.log(undefined<=0);
+console.log("===");
+
+//=== it checks datatype as well, so it is strict
+console.log("2"===2);
+
+
--- 01_basics/datatypes_conversion.js
+++ 01_basics/datatypes_conversion.js
@@ -11,4 +11,16 @@ let stringscore=String(score)
  console.log(typeof b);
  console.log( b); //it will show NaN(Not a Number)
  
+ /* ****************operations************************** */
+
+ console.log(1+"2");
+ console.log("1"+2);
+ console.log("1"+2+3);
+ console.log(1+2+3+"1");  //left to right add hoyega hmesha
+
+ console.log(-true);
+ //console.log(true+);  not allowed, it is error
+ console.log(+"");
+ 
+ 
  
